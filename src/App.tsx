a81bdae378454dee5c43534091d15225cabe11ed
Demis Russo <demisjunior@hotmail.com>
2026-04-25 19:40:00 -0300

    add help-desk style guide

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 export function App() {
-	return <div className="container"></div>
+	return (
+		<div className="container flex h-screen w-full bg-gray-900">
+			<h1 className="text-blue-dark text-heading-lg font-lato">hello</h1>
+		</div>
+	)
 }
 
 export default App
